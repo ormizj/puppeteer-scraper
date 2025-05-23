6a0b709c4988f73dac6e27c7eb4108ce6a165614
spiderpig60 <spiderpig60@gmail.com>
2025-05-23 13:35:52 +0300

    master config

--- src/services/EnvConfig.ts
+++ src/services/EnvConfig.ts
@@ -1,22 +1,13 @@
 export class EnvConfig {
-  private static instance: EnvConfig;
-
-  private constructor() {}
-
-  public static getInstance(): EnvConfig {
-    if (!EnvConfig.instance) EnvConfig.instance = new EnvConfig();
-    return EnvConfig.instance;
-  }
-
-  public get(key: ConfigKeyNames): string | number {
+  public static get<K extends ConfigKeyNames>(key: K): ConfigKeys[K] {
     const value = process.env[key];
-
     if (value === undefined || value === "") {
       throw new Error(
         `Environment variable '${key}' is not defined or is empty`,
       );
     }
 
-    return value;
+    const numValue = Number(value);
+    return (isNaN(numValue) ? value : numValue) as ConfigKeys[K];
   }
 }
