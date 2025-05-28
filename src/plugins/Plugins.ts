b50ef5f250aa3851620e7df6a94c72b3f6337c08
spiderpig60 <spiderpig60@gmail.com>
2025-05-28 23:48:07 +0300

    master docs

--- src/plugins/Plugins.ts
+++ src/plugins/Plugins.ts
@@ -3,5 +3,6 @@ import { databasePlugin } from "./Database.ts";
 export const initializePlugins = () => {
   // creates the database, and closes the connection
   databasePlugin()();
+  // cleanup function
   return () => {};
 };
